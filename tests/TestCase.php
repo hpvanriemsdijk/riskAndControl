6b96611f6f7a9d559ff2e08ad2b607954e7cde1b
Hans-Peter van Riemsdijk <hans-peter.vanriemsdijk@live.nl>
2015-09-28 06:56:31 -0400

    Setup coverall

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      * @var string
      */
     protected $baseUrl = 'http://localhost';
+
     /**
      * Creates the application.
      *
@@ -15,6 +16,8 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
     public function createApplication()
     {
         $app = require __DIR__.'/../bootstrap/app.php';
+        
+
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
         return $app;
     }
